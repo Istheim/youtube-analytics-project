999d0d13e56e95c62f68aa07981892b1d9c05cf2
Matvey Zaytsev(Istheim) <zaytsev_matvey98@mail.ru>
2023-05-22 18:41:18 +0700

    add all

--- src/channel.py
+++ src/channel.py
@@ -1,12 +1,30 @@
+import os
+import requests
+import json
 
 
 class Channel:
-    """Класс для ютуб-канала"""
+    def __init__(self, channel_id):
+        self.channel_id = channel_id
 
-    def __init__(self, channel_id: str) -> None:
-        """Экземпляр инициализируется id канала. Дальше все данные будут подтягиваться по API."""
-        pass
+    def print_info(self):
+        api_key = os.getenv("YT_API_KEY")
+        url = f"https://www.googleapis.com/youtube/v3/channels?part=snippet,statistics&id={self.channel_id}&key={api_key}"
+        response = requests.get(url)
+        data = json.loads(response.text)
 
-    def print_info(self) -> None:
-        """Выводит в консоль информацию о канале."""
-        pass
+        if "items" in data and len(data["items"]) > 0:
+            channel = data["items"][0]
+            snippet = channel["snippet"]
+            statistics = channel["statistics"]
+
+            print(f"Title: {snippet['title']}")
+            print(f"Description: {snippet['description']}")
+            print(f"Custom URL: {snippet['customUrl']}")
+            print(f"Published At: {snippet['publishedAt']}")
+            print(f"View Count: {statistics['viewCount']}")
+            print(f"Subscriber Count: {statistics['subscriberCount']}")
+            print(f"Video Count: {statistics['videoCount']}")
+        else:
+            print("Channel not found")
+
